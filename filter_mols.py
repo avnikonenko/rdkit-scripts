8211958b440ae17b39d7e5097ad67264243eb9f4
Pavel Polishchuk <pavel_polishchuk@ukr.net>
2026-05-28 22:47:53 +0200

    Add optional ordering of molecules in filer_mols.py

--- filter_mols.py
+++ filter_mols.py
@@ -5,19 +5,35 @@ __author__ = 'Pavel Polishchuk'
 import argparse
 
 
-def filter_mols(input_fname, output_fname, names_fname):
+def filter_mols(input_fname, output_fname, names_fname, order):
 
     with open(names_fname)as f:
-        names = {line.strip() for line in f}
+        names = []
+        names_set = set()
+        for line in f:
+            name = line.strip()
+            if name not in names_set:
+                names.append(name)
+                names_set.add(name)
 
     with open(input_fname) as f_in, open(output_fname, 'wt') as f_out:
         molstr = []
+        mols_by_name = {} if order else None
         for line in f_in:
             molstr.append(line)
             if line.strip() == '$$$$':
-                if molstr[0].strip() in names:
-                    f_out.writelines(molstr)
+                name = molstr[0].strip()
+                if name in names_set:
+                    if order:
+                        mols_by_name.setdefault(name, []).append(molstr)
+                    else:
+                        f_out.writelines(molstr)
                 molstr = []
+
+        if order:
+            for name in names:
+                for molstr in mols_by_name.get(name, []):
+                    f_out.writelines(molstr)
 
 
 def main():
@@ -28,9 +44,12 @@ def main():
                         help='output SDF file.')
     parser.add_argument('-n', '--names', metavar='FILENAME', required=True, type=str,
                         help='text file with molecule names to keep.')
+    parser.add_argument('--ordered', action='store_true',
+                        help='return molecules in the order specified in the names file. By default input order is '
+                             'preserved.')
     args = parser.parse_args()
 
-    filter_mols(args.input, args.output, args.names)
+    filter_mols(args.input, args.output, args.names, args.order)
 
 
 if __name__ == '__main__':
